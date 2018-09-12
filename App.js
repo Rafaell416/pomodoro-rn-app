b8b7e09ba02bc810ccb3bd95e17527e28b2d6e73
Rafel Villarreal <rvillarreal416@gmail.com>
2018-09-12 15:38:00 -0500

    cache images assets

--- App.js
+++ App.js
@@ -1,8 +1,48 @@
 import React, { Component } from 'react'
 import AppNavigation from './src/Navigation'
+import { Image } from 'react-native'
+import {
+  Asset,
+  AppLoading
+} from 'expo'
+
+function _cacheImages (images) {
+  return images.map(image => {
+    if (typeof image === 'string') return Image.prefetch(image)
+
+    return Asset.fromModule(image).downloadAsync()
+  })
+}
 
 export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      isReady: false
+    }
+  }
+
+  componentWillMount(){
+    console.disableYellowBox = true
+  }
+
+  _loadAssetsAsync = async () => {
+    const images = await _cacheImages([
+      require('./assets/app.png')
+    ])
+  }
+
+
   render() {
-    return <AppNavigation />
+    const { isReady } = this.state
+    if (isReady) return <AppNavigation />
+
+    return (
+      <AppLoading
+        startAsync={this._loadAssetsAsync}
+        onFinish={()=>this.setState({ isReady: true })}
+        onError={(err)=>console.warn(err)}
+      />
+    )
   }
 }
